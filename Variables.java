71de10483d3834b4fa541609c877540934e2ba2c
Naveenkumar Govindasamy <45295606+naveengovindasamy@users.noreply.github.com>
2025-03-29 18:41:49 -0400

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,92 @@
+
+//(1). Create a string variable
+//public class Variables {
+//	public static void main(String[] args) {
+//		String computer = "msi";
+//		System.out.println(computer);
+//	}
+//	
+//	
+//	
+//}
+
+
+//(2). Create an integer variable
+//public class Variables{
+//	public static void main(String[] args) {
+//		int thala = 7;
+//		System.out.println(thala);
+//	}
+//}
+
+
+//(3).Create a variable without assigning the value, and assign the value later
+
+//public class Variables{
+//	public static void main(String[] args) {
+//		int num;
+//		num = 10;
+//		System.out.println(num);
+//	}
+//}
+
+
+//(4).Overwrite an existing variable value
+
+//public class Variables{
+//	public static void main(String args[]) {
+//		String name = "naveen";
+//		name = "kumar";
+//		System.out.println(name);
+//	}
+//}
+
+
+//(5). Create a final variable(unchangeable and read-only)
+//public class Variables{
+//	public static void main(String args[]) {
+//		final int number = 15;
+//		//number = 5; 
+//		System.out.println(number);
+//			
+//	}
+//}
+
+//will throw error as final number is declared as 15
+
+
+//(6).Combine text and a variable on display
+
+//public class Variables{
+//	public static void main(String[] args) {
+//		int no = 5;
+//		String text = "what";
+//		System.out.println(text + no);
+//	}
+//}
+
+//(7).Add a variable to another variable
+
+//public class Variables{
+//	public static void main(String[] args) {
+//		int variable1 = 2;
+//				int variable2 = 4;
+//				System.out.println(variable1 + variable2);
+//	}
+//}
+
+//(8).Declare many varibles of same type with a comma-separated list
+
+//public class Variables{
+//	public static void main(String[] args) {
+//		String A = "android", B = "Black";
+//		System.out.println(A + B);
+//	}
+//}
+
+
+
+
+
+
+
